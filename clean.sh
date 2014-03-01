884ca8f2727e2774990d48e3a413a85b119988ac
Adrian-Tudor Panescu <adrian@panescu.com>
2014-03-01 18:42:46 +0100

    Various additions
    * Listing, table, cleanup script.
    
    * Clean trailing whitespace.
    
    * Ignore PDF files.
    
    * Master document markers.
    
    Signed-off-by: Adrian-Tudor Panescu <adrian@panescu.com>

---
+++ clean.sh
@@ -0,0 +1,15 @@
+# this script will clean the auxiliary files generated when compiling latex
+
+rm *.aux
+rm *.bbl
+rm *.blg
+rm *.fdb_latexmk
+rm *.fls
+rm *.log
+rm *.out
+rm *.toc
+rm *.idx
+rm *.lof
+rm *.bak
+rm static/img/*.pdf
+#rm *.pdf # you may want to keep the final output
